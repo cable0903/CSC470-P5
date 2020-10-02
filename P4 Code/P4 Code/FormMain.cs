7802dd27f4ccd50bfb300e6cc115c4edc28bcd58
bhimr <35665192+bhimr@users.noreply.github.com>
2020-10-02 00:11:47 -0500

    Login functions added

--- P4 Code/P4 Code/FormMain.cs
+++ P4 Code/P4 Code/FormMain.cs
@@ -12,19 +12,38 @@ namespace P4_Code
 {
     public partial class FormMain : Form
     {
+        public AppUser LoginUser = new AppUser();
         public FormMain()
         {
             InitializeComponent();
         }
-
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+        }
         private void LoginBttn_Click(object sender, EventArgs e)
         {
+            FakeAppUserRepository test = new FakeAppUserRepository();
+            string user_name = UserNameBox.Text;
+            string password = PasswordBox.Text;
+            bool result = test.Login(user_name, password);
 
+            if (result == true)
+            {
+                test.SetAuthentication(user_name, result);
+                LoginUser = test.GetByUserName(user_name);
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect UsereName or Password. Try again!", "Attention");
+            }
         }
 
         private void CancelBttn_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
     }
 }
