73c3c00fd6df40cb5717a557a2e76737ecbd8c03
Bethany Cable <bethany.cable@trojans.dsu.edu>
2020-10-08 18:34:20 -0500

    Renamed project files and any namespaces that were named P4.

---
+++ P5 Code/P5 Code/FormLogin.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace P5_Code
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserNameBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginBttn = new System.Windows.Forms.Button();
+            this.CancelBttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UserNameBox
+            // 
+            this.UserNameBox.Location = new System.Drawing.Point(179, 67);
+            this.UserNameBox.Name = "UserNameBox";
+            this.UserNameBox.Size = new System.Drawing.Size(266, 20);
+            this.UserNameBox.TabIndex = 0;
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(179, 108);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.PasswordChar = '*';
+            this.PasswordBox.Size = new System.Drawing.Size(266, 20);
+            this.PasswordBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // LoginBttn
+            // 
+            this.LoginBttn.Location = new System.Drawing.Point(338, 170);
+            this.LoginBttn.Name = "LoginBttn";
+            this.LoginBttn.Size = new System.Drawing.Size(107, 35);
+            this.LoginBttn.TabIndex = 4;
+            this.LoginBttn.Text = "Login";
+            this.LoginBttn.UseVisualStyleBackColor = true;
+            this.LoginBttn.Click += new System.EventHandler(this.LoginBttn_Click);
+            // 
+            // CancelBttn
+            // 
+            this.CancelBttn.Location = new System.Drawing.Point(179, 170);
+            this.CancelBttn.Name = "CancelBttn";
+            this.CancelBttn.Size = new System.Drawing.Size(107, 35);
+            this.CancelBttn.TabIndex = 5;
+            this.CancelBttn.Text = "Cancel";
+            this.CancelBttn.UseVisualStyleBackColor = true;
+            this.CancelBttn.Click += new System.EventHandler(this.CancelBttn_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 283);
+            this.Controls.Add(this.CancelBttn);
+            this.Controls.Add(this.LoginBttn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.UserNameBox);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserNameBox;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button LoginBttn;
+        private System.Windows.Forms.Button CancelBttn;
+    }
+}
+
---
+++ P5 Code/P5 Code/FormLogin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_Code
+{
+    public partial class FormLogin : Form
+    {
+        public AppUser LoginUser = new AppUser();
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+        }
+        private void LoginBttn_Click(object sender, EventArgs e)
+        {
+            FakeAppUserRepository test = new FakeAppUserRepository();
+            string user_name = UserNameBox.Text;
+            string password = PasswordBox.Text;
+            bool result = test.Login(user_name, password);
+
+            if (result == true)
+            {
+                test.SetAuthentication(user_name, result);
+                LoginUser = test.GetByUserName(user_name);
+                FormMain login = new FormMain();
+                this.Hide();
+                login.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect UsereName or Password. Try again!", "Attention");
+            }
+        }
+
+        private void CancelBttn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
