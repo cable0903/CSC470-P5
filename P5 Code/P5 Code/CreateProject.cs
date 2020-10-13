9198ddf98ed26b98ca45c76925875a002f41f97e
Bethany Cable <bethany.cable@trojans.dsu.edu>
2020-10-12 22:56:24 -0500

    Coded add button to prevent empty textbox and a message box. Added remove trailing blanks and coded cancel to close form.

--- P5 Code/P5 Code/CreateProject.cs
+++ P5 Code/P5 Code/CreateProject.cs
@@ -12,6 +12,7 @@ namespace P5_Code
 {
     public partial class CreateProject : Form
     {
+        string newProjectName;
         public CreateProject()
         {
             InitializeComponent();
@@ -24,17 +25,22 @@ namespace P5_Code
 
         private void projectNameText_TextChanged(object sender, EventArgs e)
         {
-
+            newProjectName = projectNameText.Text;
+            newProjectName = newProjectName.Trim();
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void addButton_Click(object sender, EventArgs e)
         {
-
+            if(projectNameText.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Project name is empty or blank.");
+                return;
+            }
         }
     }
 }
